5aaa5ef486810f37268134d648e787f8c4f5ab75
abizmo <8437730+abizmo@users.noreply.github.com>
2024-10-31 15:22:28 +0000

    fix confirmation dialog

--- src/components/order-confirmed.tsx
+++ src/components/order-confirmed.tsx
@@ -1,5 +1,7 @@
 import { useStore } from "@nanostores/react"
 import { cart, clearOrder, isConfirmed, type CartItem } from "../stores/cart"
+import "@/styles/order-confirmed.css"
+import formatPrice from "@/utils/format-price"
 
 export default function OrderConfirmed() {
   const $isConfirmed = useStore(isConfirmed)
@@ -10,32 +12,45 @@ export default function OrderConfirmed() {
     0,
   )
 
+  if (!$isConfirmed) return null
   return (
-    <dialog open={$isConfirmed}>
-      <img src="/icons/order-confirmed.svg" alt="Order confirmed" />
-      <h2>Order Confirmed</h2>
-      <p>We hope you enjoy your food!</p>
-      <div>
-        <ul>
-          {cartItems.map(({ name, image, quantity, price }: CartItem) => (
-            <li key={name}>
-              <img src={image} alt={name} />
-              <div>
-                <p>{name}</p>
-                <div>
-                  <span>{quantity}</span>
-                  <span>{price}</span>
+    <div className="confirmation-container">
+      <dialog className="confirmation" open={$isConfirmed}>
+        <img
+          className="confirmation__icon"
+          src="/icons/order-confirmed.svg"
+          alt="Order confirmed"
+        />
+        <h2 className="confirmation__title">Order Confirmed</h2>
+        <p className="confirmation__text">We hope you enjoy your food!</p>
+        <div className="order">
+          <ul className="order__list">
+            {cartItems.map(({ name, image, quantity, price }: CartItem) => (
+              <li className="order__item" key={name}>
+                <img src={image} alt={name} />
+                <div className="order__item-info">
+                  <p className="order__item-name">{name}</p>
+                  <div>
+                    <span className="order__item-qty">{quantity}x</span>
+                    <span className="order__item-price">
+                      @ {formatPrice(price)}
+                    </span>
+                  </div>
                 </div>
-              </div>
-              <span>{price * quantity}</span>
-            </li>
-          ))}
-        </ul>
-        <p>
-          Order Total <strong>{total}</strong>
-        </p>
-      </div>
-      <button onClick={clearOrder}>Start New Order</button>
-    </dialog>
+                <span className="order__item-total">
+                  {formatPrice(price * quantity)}
+                </span>
+              </li>
+            ))}
+          </ul>
+          <p className="order__total">
+            Order Total <strong>{formatPrice(total)}</strong>
+          </p>
+        </div>
+        <button className="button" onClick={clearOrder}>
+          Start New Order
+        </button>
+      </dialog>
+    </div>
   )
 }
